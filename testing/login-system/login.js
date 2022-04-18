a9e1012a3a79fb09dc81cb8c4ea8089c4c53f17b
sggsdatafornehs <102708864+sggsdatafornehs@users.noreply.github.com>
2022-04-18 11:48:44 +0800

    Update login.js

--- testing/login-system/login.js
+++ testing/login-system/login.js
@@ -11,3 +11,35 @@ window.location.href="welcome";
 alert("請輸入正確的使用者名稱和密碼！工程組在瞪你喔~!")
 }
 }
+document.onmousedown = function(e){
+    if ( e.which == 2 ){// 滑鼠滾輪的按下，滾動不觸發
+        return false;
+    }
+    if( e.which==3 ){// 滑鼠右鍵
+        return false;
+    }
+}
+document.onkeydown = function(){
+
+        if (window.event && window.event.keyCode == 123) {
+            event.keyCode = 0;
+            event.returnValue = false;
+        }
+        if (window.event && window.event.keyCode == 13) {
+            window.event.keyCode = 505;
+        }
+        if (window.event && window.event.keyCode == 8) {
+            window.event.returnValue = false;
+        }
+    
+
+    if( event.ctrlKey ){
+        return false;
+    }
+    if ( event.altKey ){
+        return false;
+    }
+    if ( event.shiftKey ){
+        return false;
+    }
+}
