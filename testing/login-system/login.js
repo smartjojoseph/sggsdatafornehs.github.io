2558fe3512f49dfbd5efe599e31f1ae2074a5464
sggsdatafornehs <102708864+sggsdatafornehs@users.noreply.github.com>
2022-04-18 15:29:57 +0800

    Update login.js

--- testing/login-system/login.js
+++ testing/login-system/login.js
@@ -25,12 +25,7 @@ document.onkeydown = function(){
             event.keyCode = 0;
             event.returnValue = false;
         }
-        if (window.event && window.event.keyCode == 13) {
-            window.event.keyCode = 505;
-        }
-        if (window.event && window.event.keyCode == 8) {
-            window.event.returnValue = false;
-        }
+
     
 
     if( event.ctrlKey ){
