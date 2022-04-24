ce0ba4cb9742b9455c22a19e59c8dffa83f006bc
sggsdatafornehs <102708864+sggsdatafornehs@users.noreply.github.com>
2022-04-24 18:31:04 +0800

    Update login.js

--- upgradeweb/login-system/login.js
+++ upgradeweb/login-system/login.js
@@ -169,7 +169,7 @@ if (username.value == "") {
 alert("請輸入使用者名稱");
 } else if (pass.value  == "") {
 alert("請輸入密碼");
-} else if(username.value == "sggs" && pass.value == "1202"){
+} else if(username.value == "sggs" && pass.value == "1202" or username.value="shaenno"&& pass.value="1110"){
 window.location.href="welcome";
 } else {
 alert("請輸入正確的使用者名稱和密碼！工程組在瞪你喔~!")
